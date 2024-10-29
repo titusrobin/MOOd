3dc738076e2975d685e1e6c3c7d82d9c14aa797b
Nick Eubank <nick@nickeubank.com>
2024-10-29 12:45:30 -0400

    upload split setup

--- 00_setup_downloads.sh
+++ 00_setup_downloads.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 # Install required system libraries
-sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx
+# sudo apt-get update
+# sudo apt-get install -y libgl1-mesa-glx
 
 # Define versions for installations
 MMCV_VERSION="1.3.14"
@@ -18,39 +19,17 @@ PETR_PATH="./PETR"          # Path for the PETR model
 # Create directory for nuScenes dataset
 mkdir -p $NUSCENES_PATH
 
-# Download and extract nuScenes mini dataset
+# # Download and extract nuScenes mini dataset
 wget https://www.nuscenes.org/data/v1.0-mini.tgz -O nuscenes_mini.tgz
 tar -xf nuscenes_mini.tgz -C $NUSCENES_PATH
 
-# Install nuScenes devkit
-pip install nuscenes-devkit==$NUSCENES_DEVKIT_VERSION
-
-# Install MMCV (CPU version)
-pip install mmcv-full==$MMCV_VERSION -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.9.0/index.html
-pip install mmsegmentation==0.17.0
-
 # Install MMDetection
 git clone https://github.com/open-mmlab/mmdetection.git
-cd mmdetection
-git checkout v$MMDET_VERSION
-pip install -r requirements/build.txt
-python setup.py develop
-cd ..
 
 # Install MMDetection3D
 git clone https://github.com/open-mmlab/mmdetection3d.git
-cd mmdetection3d
-git checkout v$MMD3D_VERSION
-pip install -r requirements/build.txt
-python setup.py develop
-cd ..
 
-# Install PETR
-# git clone https://github.com/megvii-research/PETR.git
-# cd PETR
-!git clone https://github.com/WangYueFt/detr3d.git
-%cd detr3d
-!pip install -v -e 
-cd ..
+# Clone detr3d
+git clone https://github.com/WangYueFt/detr3d.git
 
 echo "Setup complete! The nuScenes mini dataset has been downloaded and extracted."
