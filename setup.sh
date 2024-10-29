868fe1c7e0147d4fbada25e80a89dd066fd56eba
afraa-n <143756865+afraa-n@users.noreply.github.com>
2024-10-29 15:42:02 +0000

    env setup

--- setup.sh
+++ setup.sh
@@ -1,54 +1,55 @@
 #!/bin/bash
 
-# Update and install required packages
+# Install required system libraries
 sudo apt-get update
-sudo apt-get install -y python3.8 python3.8-venv python3.8-dev wget
+sudo apt-get install -y libgl1-mesa-glx
 
-# Set up a virtual environment
-python3.8 -m venv env
-source env/bin/activate
+# Define versions for installations
+MMCV_VERSION="1.4.0"
+MMDET_VERSION="2.24.1"
+MMD3D_VERSION="0.17.1"
+NUSCENES_DEVKIT_VERSION="1.1.0"  # Version for the nuScenes devkit
 
-pip install tools
-pip install torch
-git add 
-# Install MMCV without CUDA
-pip install mmcv-full==1.4.0 -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.9.0/index.html
+# Define paths
+NUSCENES_PATH="./nuscenes"  # Path to download the nuScenes dataset
+PETR_PATH="./PETR"          # Path for the PETR model
+# Download and extract nuScenes mini dataset
+
+# Create directory for nuScenes dataset
+mkdir -p $NUSCENES_PATH
+
+# Download and extract nuScenes mini dataset
+wget https://www.nuscenes.org/data/v1.0-mini.tgz -O nuscenes_mini.tgz
+tar -xf nuscenes_mini.tgz -C $NUSCENES_PATH
+
+# Install nuScenes devkit
+pip install nuscenes-devkit==$NUSCENES_DEVKIT_VERSION
+
+# Install MMCV (CPU version)
+pip install mmcv-full==$MMCV_VERSION -f https://download.openmmlab.com/mmcv/dist/cpu/torch1.9.0/index.html
 
 # Install MMDetection
 git clone https://github.com/open-mmlab/mmdetection.git
 cd mmdetection
-git checkout v2.24.1
+git checkout v$MMDET_VERSION
 pip install -r requirements/build.txt
 python setup.py develop
 cd ..
 
-# Install MMSegmentation
-pip install mmsegmentation==0.20.2
-
 # Install MMDetection3D
 git clone https://github.com/open-mmlab/mmdetection3d.git
 cd mmdetection3d
-git checkout v0.17.1
+git checkout v$MMD3D_VERSION
 pip install -r requirements/build.txt
 python setup.py develop
 cd ..
 
 # Install PETR
-git clone https://github.com/megvii-research/PETR.git
-cd PETR
-mkdir ckpts
-mkdir data
-ln -s ../mmdetection3d ./mmdetection3d
+# git clone https://github.com/megvii-research/PETR.git
+# cd PETR
+!git clone https://github.com/WangYueFt/detr3d.git
+%cd detr3d
+!pip install -v -e 
 cd ..
 
-# Download and extract the nuScenes mini dataset
-mkdir -p data/nuscenes
-cd data/nuscenes
-wget https://www.nuscenes.org/data/v1.0-mini.tgz
-tar -xvf v1.0-mini.tgz
-cd ../..
-
-# Run data preparation script for nuScenes mini dataset
-python3 mmdetection3d/tools/create_data.py nuscenes --root-path data/nuscenes --out-dir data/nuscenes --extra-tag nuscenes --version v1.0-mini
-
-echo "Setup complete. Activate your virtual environment with 'source env/bin/activate'"
+echo "Setup complete! The nuScenes mini dataset has been downloaded and extracted."
